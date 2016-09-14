1dcbb45279773bd684f72930a5db4e47277f1776
jamies12 <jamie.shaun.simpson12@gmail.com>
2016-09-14 15:11:08 +0100

    Added JS board/card

--- Memory-game1/main.js
+++ Memory-game1/main.js
@@ -2,3 +2,21 @@ var cardOne = "King";
 var cardTwo = "King";
 var cardThree = "Queen";
 var cardFour = "Queen";
+
+//if (cardOne == cardTwo) {
+//	alert ("You found a match!")
+//} else if (cardThree == cardFour) {
+//	alert ("You found a match!")
+//} else if (cardOne == !cardTwo) {
+//	alert ("Sorry, try again")
+//} else if (cardThree == !cardFour) {
+//	alert ("Sorry, try again")
+//}
+var board = document.getElementById("game-board");
+function createBoard() {
+	for (var i=0; i<cards.length; i++) {
+		var cardElement = document.createElement("div");
+		cardElement.classname = 'card';
+		board.appendChild(cardElement);
+	}
+}
