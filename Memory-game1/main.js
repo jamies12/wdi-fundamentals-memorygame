9b3840c682f34b544252fb1263b252582a68e233
jamies12 <jamie.shaun.simpson12@gmail.com>
2016-09-15 15:18:06 +0100

    Changed img links/added appendChild

--- Memory-game1/main.js
+++ Memory-game1/main.js
@@ -19,15 +19,16 @@ function createBoard() {
     cardElement.setAttribute('data-card', cards[i]);
     cardElement.addEventListener('click', isTwoCards);
     board.appendChild(cardElement);
+    board.appendChild(cardElement);
   }
 
 }
 function isTwoCards () {
   cardsInPlay.push(this.getAttribute('data-card'));
-  if ('data-card' === 'king') {
-    document.cardElement.innerHTML = '<img src="hearts-884196_1280.png"'
+  if (this.getAttribute('data-card')) === 'king' {
+    this.innerHTML = '<img src="https://s-media-cache-ak0.pinimg.com/236x/62/c7/78/62c77858f718a5298f4d8cb3ad407b43.jpg"'
   } else {
-    document.cardElement.innerHTML = '<img src="spades-884203_1280.png"'
+    this.innerHTML = '<img src="https://s-media-cache-ak0.pinimg.com/564x/9f/1a/e4/9f1ae4bc1c344873907920bd6eb8c407.jpg"'
   }
   if cardsInPlay === 2 {
     isMatch(cardsInPlay);
